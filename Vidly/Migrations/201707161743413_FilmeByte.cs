338b34285c9fe1533006db5b947d32471ba41048
Marcus Pacheco <marcus.pacheco@gmail.com>
2017-07-16 14:48:40 -0300

    Code First Migrations

--- Vidly/Migrations/201707161743413_FilmeByte.cs
+++ Vidly/Migrations/201707161743413_FilmeByte.cs
@@ -7,12 +7,10 @@ namespace Vidly.Migrations
     {
         public override void Up()
         {
-            AlterColumn("dbo.Filmes", "NumeroEmEstoque", c => c.Byte(nullable: false));
         }
         
         public override void Down()
         {
-            AlterColumn("dbo.Filmes", "NumeroEmEstoque", c => c.Int(nullable: false));
         }
     }
 }
